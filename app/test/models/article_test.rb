2710dd422239c8636787537a62c844e711c373aa
cumet04 <medalhkr@gmail.com>
2022-01-16 08:23:17 +0000

    sample test

--- app/test/models/article_test.rb
+++ app/test/models/article_test.rb
@@ -3,7 +3,9 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Without `title`, it isn't valid" do
+    article = Article.new(body: "foobarbaz999", status: "public")
+    article.title = nil
+    assert_not article.valid?
+  end
 end
